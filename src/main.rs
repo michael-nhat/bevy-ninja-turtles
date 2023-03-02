0514a709ad0a25a5d079046454e5fba526a0ef8e
Your Name <you@example.com>
2023-03-03 02:46:26 +0700

    init physic, floor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 mod camera;
 pub use camera::*;
-// use bevy_rapier2d::prelude::*;
+use bevy_rapier2d::prelude::*;
 
 #[allow(dead_code)]
 struct Postion { x:f32, y: f32 }
@@ -18,8 +18,10 @@ fn main(){
             ..Default::default()
         })
         .insert_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
         .add_startup_system(setup.system())
         .add_startup_stage("player_setup", SystemStage::single(spawn_player.system()))
+        .add_startup_stage("floor_setup", SystemStage::single(spawn_floor.system()))
         .add_plugins(DefaultPlugins)
         .run();
 }
@@ -28,13 +30,55 @@ fn setup(mut commands: Commands) {
     commands.spawn_bundle(new_camera_2d());
 }
 
-#[allow(dead_code)]
 fn spawn_player(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
+    let rigid_body = RigidBodyBundle {
+        mass_properties: RigidBodyMassPropsFlags::ROTATION_LOCKED.into(),
+        activation: RigidBodyActivation::cannot_sleep(),
+        ccd: RigidBodyCcd {
+                ccd_enabled: true,
+                ..Default::default()
+            },
+        ..Default::default()
+    };
+    let collider = ColliderBundle {
+        shape: ColliderShape::cuboid(0.5, 0.5),
+        flags: ColliderFlags {
+            active_events: ActiveEvents::CONTACT_EVENTS,
+            ..Default::default()
+        },
+        ..Default::default()
+    };
     commands
         .spawn_bundle(SpriteBundle {
             material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
             sprite: Sprite::new(Vec2::new(1.0, 1.0)),
             ..Default::default()
         })
+        .insert_bundle(rigid_body)
+        .insert_bundle(collider)
+        .insert(RigidBodyPositionSync::Discrete)
         .insert(Player);
+}
+
+fn spawn_floor(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>){
+    let width = 10.;
+    let height = 1.;
+    let rigid_body = RigidBodyBundle {
+        position: Vec2::new(0.0, -2.).into(),
+        body_type: RigidBodyType::Static,
+        ..Default::default()
+    };
+    let collider = ColliderBundle {
+        shape: ColliderShape::cuboid(width / 2., height / 2.),
+        ..Default::default()
+    };
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.add(Color::rgb(0.7, 0.7, 0.7).into()),
+            sprite: Sprite::new(Vec2::new(width, height)),
+            ..Default::default()
+        })
+        .insert(rigid_body)
+        .insert_bundle(collider)
+        .insert(RigidBodyPositionSync::Discrete);
 }
---
+++ todo.md
@@ -0,0 +1,8 @@
+#### Done:
+    - init windows
+    - init player, floor, physic 
+#### Todo:
+    - jump
+    - auto boot
+    - boot move
+
